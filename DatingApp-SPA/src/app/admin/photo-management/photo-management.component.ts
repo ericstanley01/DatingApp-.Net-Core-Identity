eb72be026056d2c85bc18846ffe669bb0ae7d9a0
eric.stanleyjones@gmail.com <eric.stanleyjones@gmail.com>
2019-08-18 19:12:13 +0530

    Added photo management solution

--- DatingApp-SPA/src/app/admin/photo-management/photo-management.component.ts
+++ DatingApp-SPA/src/app/admin/photo-management/photo-management.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Photo } from 'src/app/_models/photo';
+import { AdminService } from 'src/app/_services/admin.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-photo-management',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo-management.component.sass']
 })
 export class PhotoManagementComponent implements OnInit {
+  photos: any;
 
-  constructor() { }
+  constructor(private adminService: AdminService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+    this.getPhotosForApproval();
+  }
+
+  getPhotosForApproval() {
+    this.adminService.getPhotosForApproval().subscribe((photos) => {
+      this.photos = photos;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+
+  approvePhoto(photoId) {
+    this.adminService.approvePhoto(photoId).subscribe(() => {
+      this.photos.splice(this.photos.findIndex(p => p.id === photoId), 1);
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+
+  rejectPhoto(photoId) {
+    this.adminService.rejectPhoto(photoId).subscribe(() => {
+      this.photos.splice(this.photos.findIndex(p => p.id === photoId), 1);
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 
 }
