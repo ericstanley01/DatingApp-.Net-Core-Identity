3496184defed95b22fa8b921ade80c92f2f4d2de
eric.stanleyjones@gmail.com <eric.stanleyjones@gmail.com>
2020-05-08 22:26:18 +0530

    Environment changes

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -28,176 +28,185 @@ using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace DatingApp.API
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<DataContext>(x => {
-                x.UseMySql(Configuration.GetConnectionString("ProdConnection"))
-                .ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.IncludeIgnoredWarning));
-            });
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<DataContext>(x =>
+			{
+				x.UseMySql(Configuration.GetConnectionString("ProdConnection"))
+				.ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.IncludeIgnoredWarning));
+			});
 
-            IdentityBuilder builder = services.AddIdentityCore<User>(opt =>
-            {
-                opt.Password.RequireDigit = false;
-                opt.Password.RequiredLength = 4;
-                opt.Password.RequireNonAlphanumeric = false;
-                opt.Password.RequireUppercase = false;
-            });
+			IdentityBuilder builder = services.AddIdentityCore<User>(opt =>
+			{
+				opt.Password.RequireDigit = false;
+				opt.Password.RequiredLength = 4;
+				opt.Password.RequireNonAlphanumeric = false;
+				opt.Password.RequireUppercase = false;
+			});
 
-            builder = new IdentityBuilder(builder.UserType, typeof(Role), builder.Services);
-            builder.AddEntityFrameworkStores<DataContext>();
-            builder.AddRoleValidator<RoleValidator<Role>>();
-            builder.AddRoleManager<RoleManager<Role>>();
-            builder.AddSignInManager<SignInManager<User>>();
+			builder = new IdentityBuilder(builder.UserType, typeof(Role), builder.Services);
+			builder.AddEntityFrameworkStores<DataContext>();
+			builder.AddRoleValidator<RoleValidator<Role>>();
+			builder.AddRoleManager<RoleManager<Role>>();
+			builder.AddSignInManager<SignInManager<User>>();
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
-                            .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
-                        ValidateIssuer = false,
-                        ValidateAudience = false
-                    };
-                });
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+							.AddJwtBearer(options =>
+							{
+								options.TokenValidationParameters = new TokenValidationParameters
+								{
+									ValidateIssuerSigningKey = true,
+									IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+																									.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+									ValidateIssuer = false,
+									ValidateAudience = false
+								};
+							});
 
-            services.AddAuthorization(options => {
-                options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
-                options.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
-                options.AddPolicy("VIPOnly", policy => policy.RequireRole("VIP"));
-            });
+			services.AddAuthorization(options =>
+			{
+				options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+				options.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+				options.AddPolicy("VIPOnly", policy => policy.RequireRole("VIP"));
+			});
 
-            services.AddMvc(options =>
-                {
-                    var policy = new AuthorizationPolicyBuilder()
-                        .RequireAuthenticatedUser()
-                        .Build();
-                    options.Filters.Add(new AuthorizeFilter(policy));
-                }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
-                .AddJsonOptions(opt => {
-                    opt.SerializerSettings.ReferenceLoopHandling =
-                        Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-                });
+			services.AddMvc(options =>
+							{
+								var policy = new AuthorizationPolicyBuilder()
+																					.RequireAuthenticatedUser()
+																					.Build();
+								options.Filters.Add(new AuthorizeFilter(policy));
+							}).SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+							.AddJsonOptions(opt =>
+							{
+								opt.SerializerSettings.ReferenceLoopHandling =
+																					Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+							});
 
-            services.AddCors();
-            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
-            services.AddAutoMapper();
-            services.AddTransient<Seed>();
-            services.AddScoped<IDatingRepository, DatingRepository>();
+			services.AddCors();
+			services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
+			services.AddAutoMapper();
+			services.AddTransient<Seed>();
+			services.AddScoped<IDatingRepository, DatingRepository>();
 
-            services.AddScoped<LogUserActivity>();
-        }
+			services.AddScoped<LogUserActivity>();
+		}
 
-        public void ConfigureDevelopmentServices(IServiceCollection services)
-        {
-            services.AddDbContext<DataContext>(x =>
-            {
-                x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-            });
+		public void ConfigureDevelopmentServices(IServiceCollection services)
+		{
+			services.AddDbContext<DataContext>(x =>
+			{
+				x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+			});
 
-            IdentityBuilder builder = services.AddIdentityCore<User>(opt => {
-                opt.Password.RequireDigit = false;
-                opt.Password.RequiredLength = 4;
-                opt.Password.RequireNonAlphanumeric = false;
-                opt.Password.RequireUppercase = false;
-            });
+			IdentityBuilder builder = services.AddIdentityCore<User>(opt =>
+			{
+				opt.Password.RequireDigit = false;
+				opt.Password.RequiredLength = 4;
+				opt.Password.RequireNonAlphanumeric = false;
+				opt.Password.RequireUppercase = false;
+			});
 
-            builder = new IdentityBuilder(builder.UserType, typeof(Role), builder.Services);
-            builder.AddEntityFrameworkStores<DataContext>();
-            builder.AddRoleValidator<RoleValidator<Role>>();
-            builder.AddRoleManager<RoleManager<Role>>();
-            builder.AddSignInManager<SignInManager<User>>();
+			builder = new IdentityBuilder(builder.UserType, typeof(Role), builder.Services);
+			builder.AddEntityFrameworkStores<DataContext>();
+			builder.AddRoleValidator<RoleValidator<Role>>();
+			builder.AddRoleManager<RoleManager<Role>>();
+			builder.AddSignInManager<SignInManager<User>>();
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
-                            .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
-                        ValidateIssuer = false,
-                        ValidateAudience = false
-                    };
-                });
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+							.AddJwtBearer(options =>
+							{
+								options.TokenValidationParameters = new TokenValidationParameters
+								{
+									ValidateIssuerSigningKey = true,
+									IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+																									.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+									ValidateIssuer = false,
+									ValidateAudience = false
+								};
+							});
 
-            services.AddAuthorization(options => {
-                options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
-                options.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
-                options.AddPolicy("VIPOnly", policy => policy.RequireRole("VIP"));
-            });
+			services.AddAuthorization(options =>
+			{
+				options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+				options.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+				options.AddPolicy("VIPOnly", policy => policy.RequireRole("VIP"));
+			});
 
-            services.AddMvc(options => {
-                    var policy = new AuthorizationPolicyBuilder()
-                        .RequireAuthenticatedUser()
-                        .Build();
-                    options.Filters.Add(new AuthorizeFilter(policy));
-                })
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
-                .AddJsonOptions(opt =>
-                {
-                    opt.SerializerSettings.ReferenceLoopHandling =
-                        Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-                });
-            services.AddCors();
-            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
-            Mapper.Reset();
-            services.AddAutoMapper();
-            services.AddTransient<Seed>();
-            services.AddScoped<IDatingRepository, DatingRepository>();
+			services.AddMvc(options =>
+			{
+				var policy = new AuthorizationPolicyBuilder()
+								.RequireAuthenticatedUser()
+								.Build();
+				options.Filters.Add(new AuthorizeFilter(policy));
+			})
+							.SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+							.AddJsonOptions(opt =>
+							{
+								opt.SerializerSettings.ReferenceLoopHandling =
+																					Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+							});
+			services.AddCors();
+			services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
+			Mapper.Reset();
+			services.AddAutoMapper();
+			services.AddTransient<Seed>();
+			services.AddScoped<IDatingRepository, DatingRepository>();
 
-            services.AddScoped<LogUserActivity>();
-        }
+			services.AddScoped<LogUserActivity>();
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseExceptionHandler(builder => {
-                    builder.Run(async context => {
-                        context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
-                        var error = context.Features.Get<IExceptionHandlerFeature>();
-                        if (error != null) {
-                            context.Response.AddApplicationError(error.Error.Message);
-                            await context.Response.WriteAsync(error.Error.Message);
-                        }
-                    });
-                });
-                // app.UseHsts();
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseExceptionHandler(builder =>
+				{
+					builder.Run(async context =>
+					{
+						context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+						var error = context.Features.Get<IExceptionHandlerFeature>();
+						if (error != null)
+						{
+							context.Response.AddApplicationError(error.Error.Message);
+							await context.Response.WriteAsync(error.Error.Message);
+						}
+					});
+				});
+				// app.UseHsts();
+			}
 
-            // app.UseHttpsRedirection();
-            seeder.SeedUsers();
-            app.UseCors(x => x.AllowAnyOrigin()
-                .AllowAnyMethod().AllowAnyHeader());
-            app.UseAuthentication();
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            app.UseMvc(
-                // routes => {
-                // routes.MapSpaFallbackRoute(
-                //     name: "spa-fallback",
-                //     defaults: new { controller = "Fallback", action = "Index" }
-                // );
-                // }
-            );
-        }
-    }
+			// app.UseHttpsRedirection();
+			seeder.SeedUsers();
+			app.UseCors(x => x.AllowAnyOrigin()
+							.AllowAnyMethod().AllowAnyHeader());
+			app.UseAuthentication();
+			app.UseDefaultFiles();
+			app.UseStaticFiles();
+			app.UseMvc(
+			// routes => {
+			// routes.MapSpaFallbackRoute(
+			//     name: "spa-fallback",
+			//     defaults: new { controller = "Fallback", action = "Index" }
+			// );
+			// }
+			);
+		}
+	}
 }
